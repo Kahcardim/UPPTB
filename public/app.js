1e406cd017707e652347bee4ed3a1c58ffffddb7
Kahcardim <nexlhz@gmail.com>
2026-09-26 12:41:00 -0300

    fix(ct04): alinha schema ativo do mapa v9

--- public/app.js
+++ public/app.js
@@ -2,7 +2,7 @@ import { initRouter } from './router.js';
 import { initRandomizer } from './randomizer.js';
 
 const activeStorageKeys = new Set([
-  'upptb-campus-distribution-v8',
+  'upptb-campus-distribution-v9',
   'upptb-alice-characters-v2'
 ]);
 
